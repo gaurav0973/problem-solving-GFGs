7167f05a9d6bd85dc7e2dc3ea8e52ef6220db4e2
Gaurav Kumar Maurya <gauravKmaurya09@gmail.com>
2026-01-15 18:50:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Candy/candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minCandy(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+        
+        vector<int> candies(n, 1);
+        
+        // Left pass
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        
+        // Right pass
+        for (int i = n-2; i >= 0; i--) {
+            if (arr[i] > arr[i+1]) {
+                candies[i] = max(candies[i], candies[i+1] + 1);
+            }
+        }
+        
+        // Sum candies
+        int total = 0;
+        for (int c : candies) total += c;
+        return total;
+    }
+};
